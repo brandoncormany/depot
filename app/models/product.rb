f6e7cde20b4c9686168ce956969686883af4faeb
Brandon Cormany <brandoncormany@gmail.com>
2012-06-04 22:44:30 -0700

    Validation!
    Chapter 7 in the book

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true 
+#  validates_uniqueness_of :title, :case_sensitive => false, :message => 'Try another one dummy'
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)$}i,
+    message: 'must be a URL with GIF, JPG or PNG image.'
+  }
 end
