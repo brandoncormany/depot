f0954727f6cb64919064cf6aedbce9a18332d49a
Brandon Cormany <brandoncormany@gmail.com>
2012-06-10 22:15:33 -0700

    Bad Commit with incorrect files
    Don't use this Commit incorrect Scaffold wrote as CARD insteast of CART

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  private
+  def current_cart
+    Cart.find(session[:card_id])
+    rescue ActiveRecord::RecordNotFound
+      card = Cart.create
+      session[:card_id] = cart.id
+      cart
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Depot::Application.routes.draw do
+  resources :line_items
+
+  resources :cards
+
   get "store/index"
 
   resources :products
