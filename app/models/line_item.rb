55ea9904537323ce78c5190fb9979e207cc7a339
Brandon Cormany <brandoncormany@gmail.com>
2012-06-15 11:10:07 -0700

    Chapter 10
    Agile Web Development working Chapter 10

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,10 @@
 class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
-  attr_accessible :cart_id, :product_id
+  attr_accessible :cart_id, :product_id, :quantity
+  
+  def total_price
+    product.price * quantity
+  end
+  
 end
